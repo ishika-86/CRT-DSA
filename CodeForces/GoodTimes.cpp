d49903184e28bf18794bc3502387788bad9569f9
Ishika Bansal <ishikabansal523@gmail.com>
2026-07-01 11:27:53 +0530

    contest 1107 Div 3 problems

---
+++ CodeForces/GoodTimes.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int t; cin >> t;
+
+    while (t--) {
+        long long x; cin >> x;
+        int y=1;
+        long long temp = x;
+        while(temp>0) {
+            y *= 10;
+            temp /= 10;
+        }
+        cout << y+1 << "\n";
+    }
+    return 0;
+}
